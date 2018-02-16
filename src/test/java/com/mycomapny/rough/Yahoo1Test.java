6b77201c260b8f7f6cadaf3c0bcaa6f874161abd
suhas141 <murthy.su@husky.neu.edu>
2018-02-15 21:53:46 -0800

    initial commit

---
+++ src/test/java/com/mycomapny/rough/Yahoo1Test.java
@@ -0,0 +1,16 @@
+package com.mycomapny.rough;
+
+import org.testng.annotations.Test;
+
+public class Yahoo1Test {
+	
+	//mvn clean - cleans all src and targt
+	//mvn compile - comapile main java file
+	// mvn test -  
+	
+	@Test
+	public void letstest() {
+		
+	}
+
+}
